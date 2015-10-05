ed36815d6686d5401c124b88003da6754cfd40c5
Simon Hicks <code@simonhicks.org>
2015-10-05 19:41:55 +0100

    asdf

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,7 +5,13 @@
         :plugins [
                   [environ/environ.lein "0.3.0"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-droid "0.2.0"]
+                  [lein-droid "0.4.0-alpha4"]
                   [lein-outdated "1.0.0"]]
 
-        :hooks [environ.leiningen.hooks]}}
+        :hooks [environ.leiningen.hooks]}
+
+
+ :android-user {}
+ :android-common {:android {:sdk-path "/Users/si/src/android-sdk-macosx"
+                            :target-version 22}}
+ }
